8418d8edb3c22d1fe6e3df907eb003120103721d
abhradeepbarman <abhradeep709@gmail.com>
2024-01-08 19:52:32 +0530

    js basic day1 code added

---
+++ JS/JS_BASICS_1/index.js
@@ -0,0 +1,33 @@
+// line below is used to print on the console window
+// console.log('Namaste Dunia v3');
+
+
+// Variable creation
+
+//let --> local scope
+// let a = 5;   
+// let a = 'abhra';
+// let a = true;
+// console.log(a);
+
+//var --> global scope
+// if(true) {
+//     var a = 10;  
+//     // console.log(a);
+// }
+// console.log(a);
+
+
+// const x = 13;
+// x = 6;  //--> not possible
+
+
+// Dynamic Typing 
+let a = 5;
+console.log(a);
+
+a = 'Abhra';
+console.log(a);
+
+
+
