9a9eb2a91d9cc60b9a53ca859174919c3812efbb
abhradeepbarman <abhradeep709@gmail.com>
2024-01-11 20:15:33 +0530

    js basics day-1 code updated

--- JS/JS_BASICS_1/index.js
+++ JS/JS_BASICS_1/index.js
@@ -2,39 +2,63 @@
 // console.log('Namaste Dunia v3');
 
 
+
 // Variable creation
 
-//let --> local scope
+//let --> local scope, re-declaration not possible
 // let a = 5;   
-// let a = 'abhra';
-// let a = true;
+// let name = 'abhra';
+// let ans = true;
 // console.log(a);
 
-//var --> global scope
-// if(true) {
+
+
+//var --> global scope, re-declaration possible
+// {
 //     var a = 10;  
-//     // console.log(a);
+//     console.log(a);
 // }
 // console.log(a);
+
 
 
 // const x = 13;
 // x = 6;  //--> not possible
 
 
+
 // Dynamic Typing 
-let a = 5;
-console.log(a);
-
-a = 'Abhra';
-console.log(a);
-
-
-// let a = 6;
-// // let a = 10;  // not possible - let redeclaration
-
-// var b = 10;
-// var b = 15;  //possible - var redeclaration
-
+// let a = 5;
 // console.log(a);
-// console.log(b);
+
+// a = 'Abhra';
+// console.log(a);
+
+
+
+//Arrays
+// let arr = [1, 'Abhra', true];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+
+// arr[3] = 1.59;   // this will add the element into the array
+// console.log(arr[3]);
+// console.log(arr);
+
+
+
+//Equality Operator
+// let num = 3;
+// let str = '3';
+
+// console.log(num == str);    //loose equality --> data-type can be diff but value same
+// console.log(num === str);   //strict equality --> data-type same & value same
+
+
+
+// logical operators with Non-Booleans 
+// console.log(false || 'Abhra');       //Abhra
+// console.log(false && 'Abhra');       //false
+// console.log(true || 'Abhra');       //true
+// console.log(true && 'Abhra');       //Abhra
